97816cf323eae87869ab1dd7022f6594181837e9
Guido Dinello <guido.dinello@gmail.com>
2023-06-15 23:00:34 -0300

    dict reader for config files

---
+++ configreader.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+def read_mapping_file(file_path: str) -> dict[str, Path]:
+    mapping = {}
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            line = line.strip()
+            # if line is empty or starts with #, ignore it
+            if line == "" or line.startswith('#'):
+                continue
+            key, value = line.split('=')
+            mapping[key] = Path(value)
+    return mapping
