b603a8c29af000bcd6811820c71dfab21fff6959
Guido Dinello <guido.dinello@gmail.com>
2025-04-13 20:54:28 -0300

    feat: use glob instead of * in test.sh

--- tests/test.sh
+++ tests/test.sh
@@ -4,11 +4,11 @@ PY_VERSION="312"
 LINE_LENGTH="80"
 
 echo "Black formatting..."
-black -t "py${PY_VERSION}" -l "${LINE_LENGTH}" --exclude venv/ */*.py
+black -t "py${PY_VERSION}" -l "${LINE_LENGTH}" --exclude venv/ ./*glob*.py
 echo "Running mypy..."
-mypy */*.py --exclude venv/ --disable-error-code no-redef
+mypy ./*glob*.py --exclude venv/ --disable-error-code no-redef
 echo "Running pylint..."
-pylint */*.py \
+pylint ./*glob*.py \
     --exit-zero \
     --max-line-length=${LINE_LENGTH} \
     --disable=missing-function-docstring \
