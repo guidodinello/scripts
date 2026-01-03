f5ed974b65738b82735ea8f39f05c6e9c17bf257
Guido Dinello <guido.dinello@gmail.com>
2026-01-03 17:09:33 -0300

    fix(setup): use venv Python interpreter in generated aliases

--- setup.sh
+++ setup.sh
@@ -36,7 +36,7 @@ function add_alias() {
         return
     fi
     echo -e "${GREEN}Adding alias for ${script} to ${shell_config}${NC}"
-    echo "alias ${script}='cd ${SCRIPTS_DIR} && python ${script}/${script}.py'" >>"${shell_config}"
+    echo "alias ${script}='cd ${SCRIPTS_DIR} && ${SCRIPTS_DIR}/.venv/bin/python ${script}/${script}.py'" >>"${shell_config}"
 }
 
 function setup_aliases() {
